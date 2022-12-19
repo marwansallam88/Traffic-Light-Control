e79330a6941c5ba72e9fc54b0c955a0aa02a8bca
Marwan Sallam <marwansallam88@gmail.com>
2022-12-19 19:21:36 +0200

    Add files via upload

---
+++ Traffic Light Control/ECUAL/Button Driver/Button_Driver.c
@@ -0,0 +1,17 @@
+/*
+ * Button_Driver.c
+ *
+ * Created: 06/12/2022 23:39:52
+ * Author: marwan
+ */ 
+//Include .h
+#include "Button_Driver.h"
+
+//All button function definitions
+void Button_init(uint8_t buttonPort,uint8_t buttonPin){
+	DIO_init(buttonPort,buttonPin,IN);
+}
+
+void Button_read(uint8_t buttonPort,uint8_t buttonPin,uint8_t* value){
+	DIO_read(buttonPort,buttonPin,value);
+}
---
+++ Traffic Light Control/ECUAL/Button Driver/Button_Driver.h
@@ -0,0 +1,23 @@
+/*
+ * Button_Driver.h
+ *
+ * Created: 06/12/2022 23:40:23
+ * Author: marwan
+ */ 
+#ifndef BUTTON_DRIVER_H_
+#define BUTTON_DRIVER_H_
+
+//Include .h
+#include "../../MCAL/DIO Driver/DIO_Driver.h"
+#include "../../MCAL/Timer Driver/Timer_Driver.h"
+#include "../../MCAL/Interrupts/Interrupts.h"
+
+//Button port and pin
+#define Button_Port PORT_D
+#define Button_Pin PIN2
+
+//All button function prototypes
+void Button_init(uint8_t buttonPort,uint8_t buttonPin);//Initialize button
+void Button_read(uint8_t buttonPort,uint8_t buttonPin,uint8_t* value);//Read button
+
+#endif /* BUTTON_DRIVER_H_ */
