6b9e5519eed9cb65288a0f963c5a76ab24496ff6
Marwan Sallam <marwansallam88@gmail.com>
2022-12-19 19:24:40 +0200

    Add files via upload

---
+++ Traffic Light Control/MCAL/Timer Driver/Timer_Driver.c
@@ -0,0 +1,57 @@
+/*
+ * Timer_Driver.c
+ *
+ * Created: 06/12/2022 23:39:36
+ * Author: marwan
+ */ 
+//Include .h
+#include "Timer_Driver.h"
+
+//All timer function definitions
+void Timer_init(){
+	TCCR0 = 0x00; //normal mode
+}
+
+void Timer_delay(uint16_t ms){
+	uint16_t OFNum,initTimer;
+	double maxDelay,tic;
+	uint32_t OFC = 0;
+	//max delay 256 micro second
+	//at 1MHz no pre-scaler
+	//256 pre-scaler
+	tic = 256.0/1000.0; //ms    tic = Pre-scaler/FCPU
+	maxDelay = 65.536; //ms		Tmaxdelay = tic * 2^8
+	if(ms < maxDelay)
+	{
+		initTimer = (maxDelay - ms)/tic;
+		OFNum = 1;
+	}
+	else 
+	{
+		if(ms > maxDelay)
+		{
+			OFNum = ceil((double)ms/maxDelay);
+			initTimer = (1<<8) - ((double)ms/tic)/OFNum;
+		
+		}
+		else
+		{
+			initTimer = 0;
+			OFNum = 1;
+		}
+		
+	}
+	TCNT0 = initTimer;
+	TCCR0 |= (1<<2); //set 256 pre-scaler
+	while(OFC < OFNum)
+	{
+		//busy wait
+		while((READ_BIT(TIFR,0)) == 0);
+		//clear overflow flag
+		SET_BIT(TIFR,0);
+		//increment counter
+		OFC++;
+	}
+	//Timer stop
+	TCCR0 = 0x00;
+}
---
+++ Traffic Light Control/MCAL/Timer Driver/Timer_Driver.h
@@ -0,0 +1,19 @@
+/*
+ * Timer_Driver.h
+ *
+ * Created: 06/12/2022 23:39:05
+ * Author: marwan
+ */ 
+#ifndef TIMER_DRIVER_H_
+#define TIMER_DRIVER_H_
+
+//Include .h
+#include "../../Utilities/Registers.h"
+#include <math.h>
+#include "../../Utilities/Bit_Manipulation.h"
+
+//All timer function prototypes
+void Timer_init();//Initialize timer
+void Timer_delay(uint16_t ms);//Calculate delay
+
+#endif /* TIMER_DRIVER_H_ */
