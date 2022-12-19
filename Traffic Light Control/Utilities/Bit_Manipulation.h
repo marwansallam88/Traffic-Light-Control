8703da51f6fa72d72ed1454a6e2946ddcb245482
Marwan Sallam <marwansallam88@gmail.com>
2022-12-19 19:26:43 +0200

    Add files via upload

---
+++ Traffic Light Control/Utilities/Bit_Manipulation.h
@@ -0,0 +1,16 @@
+/*
+ * Bit_Manipulation.h
+ *
+ * Created: 06/12/2022 23:42:49
+ * Author: marwan
+ */ 
+#ifndef BIT_MANIPULATION_H_
+#define BIT_MANIPULATION_H_
+
+//All bit manipulation macros
+#define CLEAR_BIT(REG, NUM) REG&=~(1<<NUM)//Clearing bit in register to 0
+#define SET_BIT(REG, NUM) REG |= (1<<NUM)//Setting bit in register to 1
+#define TOGGLE_BIT(REG, NUM) REG ^= (1<<NUM)//Changing bit state in register
+#define READ_BIT(REG, NUM) ((REG & (1<<NUM))>>NUM)//Read bit in register
+
+#endif /* BIT_MANIPULATION_H_ */
---
+++ Traffic Light Control/Utilities/Types.h
@@ -0,0 +1,15 @@
+/*
+ * Types.h
+ *
+ * Created: 06/12/2022 23:33:03
+ * Author: marwan
+ */ 
+#ifndef TYPES_H_
+#define TYPES_H_
+
+//All typedef definitions
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+
+#endif /* TYPES_H_ */
