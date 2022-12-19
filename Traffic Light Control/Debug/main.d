27f591f6c9f6178bf02fd158baf227e7258b473b
Marwan Sallam <marwansallam88@gmail.com>
2022-12-19 19:32:45 +0200

    Add files via upload

---
+++ Traffic Light Control/Debug/main.d
@@ -0,0 +1,48 @@
+main.d main.o: .././main.c .././Application/Application.h \
+ .././Application/../ECUAL/LED\ Driver/LED_Driver.h \
+ .././Application/../ECUAL/LED\ Driver/../../MCAL/DIO\ Driver/DIO_Driver.h \
+ .././Application/../ECUAL/LED\ Driver/../../MCAL/DIO\ Driver/../../Utilities/Registers.h \
+ .././Application/../ECUAL/LED\ Driver/../../MCAL/DIO\ Driver/../../Utilities/Types.h \
+ .././Application/../ECUAL/LED\ Driver/../../MCAL/DIO\ Driver/../../Utilities/Bit_Manipulation.h \
+ .././Application/../ECUAL/LED\ Driver/../../MCAL/Timer\ Driver/Timer_Driver.h \
+ .././Application/../ECUAL/LED\ Driver/../../MCAL/Timer\ Driver/../../Utilities/Registers.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
+ .././Application/../ECUAL/LED\ Driver/../../MCAL/Timer\ Driver/../../Utilities/Bit_Manipulation.h \
+ .././Application/../ECUAL/LED\ Driver/../../MCAL/Interrupts/Interrupts.h \
+ .././Application/../ECUAL/LED\ Driver/../../MCAL/Interrupts/../../Utilities/Registers.h \
+ .././Application/../ECUAL/Button\ Driver/Button_Driver.h \
+ .././Application/../ECUAL/Button\ Driver/../../MCAL/DIO\ Driver/DIO_Driver.h \
+ .././Application/../ECUAL/Button\ Driver/../../MCAL/Timer\ Driver/Timer_Driver.h \
+ .././Application/../ECUAL/Button\ Driver/../../MCAL/Interrupts/Interrupts.h
+
+.././Application/Application.h:
+
+.././Application/../ECUAL/LED\ Driver/LED_Driver.h:
+
+.././Application/../ECUAL/LED\ Driver/../../MCAL/DIO\ Driver/DIO_Driver.h:
+
+.././Application/../ECUAL/LED\ Driver/../../MCAL/DIO\ Driver/../../Utilities/Registers.h:
+
+.././Application/../ECUAL/LED\ Driver/../../MCAL/DIO\ Driver/../../Utilities/Types.h:
+
+.././Application/../ECUAL/LED\ Driver/../../MCAL/DIO\ Driver/../../Utilities/Bit_Manipulation.h:
+
+.././Application/../ECUAL/LED\ Driver/../../MCAL/Timer\ Driver/Timer_Driver.h:
+
+.././Application/../ECUAL/LED\ Driver/../../MCAL/Timer\ Driver/../../Utilities/Registers.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+
+.././Application/../ECUAL/LED\ Driver/../../MCAL/Timer\ Driver/../../Utilities/Bit_Manipulation.h:
+
+.././Application/../ECUAL/LED\ Driver/../../MCAL/Interrupts/Interrupts.h:
+
+.././Application/../ECUAL/LED\ Driver/../../MCAL/Interrupts/../../Utilities/Registers.h:
+
+.././Application/../ECUAL/Button\ Driver/Button_Driver.h:
+
+.././Application/../ECUAL/Button\ Driver/../../MCAL/DIO\ Driver/DIO_Driver.h:
+
+.././Application/../ECUAL/Button\ Driver/../../MCAL/Timer\ Driver/Timer_Driver.h:
+
+.././Application/../ECUAL/Button\ Driver/../../MCAL/Interrupts/Interrupts.h:
---
+++ Traffic Light Control/Debug/makedep.mk
@@ -0,0 +1,16 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+Application\Application.c
+
+ECUAL\Button Driver\Button_Driver.c
+
+ECUAL\LED Driver\LED_Driver.c
+
+main.c
+
+MCAL\DIO Driver\DIO_Driver.c
+
+MCAL\Timer Driver\Timer_Driver.c
+
